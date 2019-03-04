a1479bad4963136da388546dc3f5b2dc86a1ae3f
Ayoub Lebhal <lebhal@gmail.com>
2019-03-04 14:39:43 +0100

    Removing comments

--- src/main/java/com/springboot/leb/demo/Course/CourseService.java
+++ src/main/java/com/springboot/leb/demo/Course/CourseService.java
@@ -14,14 +14,13 @@ public class CourseService {
 
 
         public List<Course> getAllCourses(){
-            //return topics;
             List<Course> courses = new ArrayList<>();
              topicRepository.findAll().forEach(courses::add);
              return courses;
         }
 
     public Course getCourse(String id) {
-          // return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+
     return topicRepository.findById(id).get();
         }
 
@@ -30,19 +29,13 @@ public class CourseService {
     }
 
     public void updateCourse(String id, Course topic) {
-       /* for (int i = 0; i <topics.size() ; i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)){
-                topics.set(i,topic);
-                return;
-            }
-        }*/
+
         topicRepository.save(topic);
 
     }
 
     public void deleteCourse(String id) {
-          //  topics.removeIf(t->t.getId().equals(id));
+
             topicRepository.deleteById(id);
     }
 }
